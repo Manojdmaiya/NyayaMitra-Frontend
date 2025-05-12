4542ea72c3ed4fbb47039c654a2e3cbcf3cdc544
Manojdmaiya <manojdmaiya@gmail.com>
2025-05-13 01:20:43 +0530

    logo-change-header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,9 +31,9 @@ const Header = () => {
                     <nav className="navbar navbar-expand-lg bg-white shadow-sm">
                         <div className="container d-flex justify-content-between align-items-center">
                             <div className="d-flex align-items-center gap-3">
+                                <img src="../NyayaMitralogobgr.png" className={styles.emblem} alt="Indian Emblem" />
+                                <h1 className="fw-bold mb-0">NyayaMitra</h1>
                                 <img src="../judiciary_logo-removebg-preview.png" className={styles.jlogo} alt="Judiciary Logo" />
-                                <h1 className="fw-bold mb-0">NyayMitra</h1>
-                                <img src="../emblem.jpg" className={styles.emblem} alt="Indian Emblem" />
                             </div>
 
                             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
